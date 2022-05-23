bac5345e1408d284dbbe00d9143d6854d6ef78eb
mylaptop <adpetrak@gmail.com>
2022-05-23 16:06:14 +0300

    added epilepsy mode

--- pong_main.py
+++ pong_main.py
@@ -4,9 +4,19 @@ from pong_helper import *
 ans = input('Single player: (s) || Two players (t)      ')
 while True:
     if ans.lower() == 's':
+        ep = input('Epilepsy mode? (y/n) ')
+        if ep == 'y':
+            ep_mode = True
+        else:
+            ep_mode = False
         spl = True
         break
     elif ans.lower() == 't':
+        ep = input('Epilepsy mode? (y/n) ')
+        if ep == 'y':
+            ep_mode = True
+        else:
+            ep_mode = False
         spl = False
         break
     else:
@@ -14,8 +24,12 @@ while True:
         ans = input('Single player: (s) || Two players (t)      ')
 
 
-def draw_win(char1, char2, ball):
-    WIN.fill(WHITE)
+def draw_win(char1, char2, ball, ep_mode):
+    if ep_mode:
+        lst = [random.randint(0, 255) for i in range(3)]
+        WIN.fill(tuple(lst))
+    else:
+        WIN.fill(WHITE)
     pygame.draw.rect(WIN, BLACK, char1)
     pygame.draw.rect(WIN, BLACK, char2)
     pygame.draw.rect(WIN, BLACK, ball)
@@ -23,7 +37,7 @@ def draw_win(char1, char2, ball):
     pygame.display.update()
 
 
-def main(single_player):
+def main(single_player, epilepsy_mode):
     run = True
     char1 = pygame.Rect(210, HEIGHT - 50, 50, 20)
     char2 = pygame.Rect(210, 50, 50, 20)
@@ -33,7 +47,7 @@ def main(single_player):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        draw_win(char1, char2, ball)
+        draw_win(char1, char2, ball, epilepsy_mode)
         movement(char1, char2)
         handle_ball(ball, char1, char2)
         if single_player:
@@ -41,6 +55,6 @@ def main(single_player):
 
 
 if __name__ == '__main__':
-    main(spl)
+    main(spl, ep_mode)
 
 
